7e81f0bfe9145cda0e52dc3aa8df77bab9aee182
David Blinn <pair+blinnd@gmail.com>
2015-09-07 17:14:32 -0400

    update etl submission

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,10 +1,8 @@
 use std::collections::BTreeMap;
 
 pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
-	input.iter().fold(BTreeMap::new(), |tree, (scrabble_value, ref scrabble_letters)| {
-		scrabble_letters.iter().fold(tree, |mut inner_tree, letter| {
-			*inner_tree.entry(letter.to_lowercase()).or_insert(0) = *scrabble_value;
-			inner_tree
-		})
+	input.iter().fold(BTreeMap::new(), |mut tree, (scrabble_value, scrabble_letters)| {
+		for letter in scrabble_letters { tree.insert(letter.to_lowercase(), *scrabble_value); }
+		tree
 	})
 }
