878661b46279ee71a1cf618f2530683e76c27224
David Blinn <pair+blinnd@gmail.com>
2015-09-07 16:56:04 -0400

    implement rust etl exercise

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,5 +1,10 @@
-pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<i32, Vec<String>> {
-	input.iter().fold(BTreeMap::new(), |mut acc, &(n, ref v)| {
+use std::collections::BTreeMap;
 
-	}
+pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
+	input.iter().fold(BTreeMap::new(), |tree, (scrabble_value, ref scrabble_letters)| {
+		scrabble_letters.iter().fold(tree, |mut inner_tree, letter| {
+			*inner_tree.entry(letter.to_lowercase()).or_insert(0) = *scrabble_value;
+			inner_tree
+		})
+	})
 }
