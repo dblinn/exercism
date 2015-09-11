6b8ccea2af1efea2b9f02d719252a0e6ec810bb4
David Blinn <pair+blinnd@gmail.com>
2015-09-10 22:33:20 -0400

    update solution to raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,21 +1,6 @@
 pub fn raindrops(number: u32) -> String {
-    let factor_sounds = [
-        (number % 3 == 0, "Pling"),
-        (number % 5 == 0, "Plang"),
-        (number % 7 == 0, "Plong"),
-    ];
-
-    if factor_sounds.iter().any(|&(should_play, _)| should_play) {
-        composite_sounds(&factor_sounds)
-    }
-    else {
-        number.to_string()
-    }
-}
-
-fn composite_sounds(factor_sounds: &[(bool, &str)]) -> String {
-    factor_sounds.iter().fold(String::new(), |composite_sound, &(should_play, factor_sound)|
-        if should_play { composite_sound + factor_sound }
-        else { composite_sound }
-    )
+    let sound = [(3, "Pling"), (5, "Plang"), (7, "Plong")].iter()
+        .map(|&(factor, sound)| if number % factor == 0 { sound } else { "" } )
+        .collect::<String>();
+    if sound.is_empty() { number.to_string() } else { sound }
 }
