9b9967bf99d8716f3c3c0e0f1bac346f7d7dad92
David Blinn <pair+blinnd@gmail.com>
2015-09-12 15:02:59 -0400

    update solution to bob exercise

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,6 +1,14 @@
 pub fn reply<'a>(prompt: &'a str) -> &'a str {
     if prompt.is_empty() { "Fine. Be that way!" }
-    else if prompt.chars().last().unwrap() == '?' { "Sure." }
-    else if prompt.chars().all(|c| !c.is_lowercase()) { "Whoa, chill out!" }
+    else if is_question(prompt) { "Sure." }
+    else if is_shouting(prompt) { "Whoa, chill out!" }
     else { "Whatever." }
+}
+
+fn is_question(prompt: &str) -> bool {
+    prompt.ends_with('?')
+}
+
+fn is_shouting(prompt: &str) -> bool {
+    prompt.chars().all(|c| !c.is_lowercase())
 }
--- rust/bob/tests/bob.rs
+++ rust/bob/tests/bob.rs
@@ -26,25 +26,21 @@ fn test_shout_numbers() {
 }
 
 #[test]
-#[ignore]
 fn test_shout_weird_characters() {
     assert_eq!("Whoa, chill out!", bob::reply("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"));
 }
 
 #[test]
-#[ignore]
 fn test_shout_without_punctuation() {
     assert_eq!("Whoa, chill out!", bob::reply("I HATE YOU"));
 }
 
 #[test]
-#[ignore]
 fn test_shout_without_question_mark() {
     assert_eq!("Whatever.", bob::reply("Ending with ? means a question."));
 }
 
 #[test]
-#[ignore]
 fn test_silent_treatment() {
     assert_eq!("Fine. Be that way!", bob::reply(""));
 }
