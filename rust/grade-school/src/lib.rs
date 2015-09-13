451f3c36027764b328ba143509d95af2361b3e16
David Blinn <pair+blinnd@gmail.com>
2015-09-12 21:12:49 -0400

    add solution to grade-school exercise

---
+++ rust/grade-school/src/lib.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+pub struct School {
+    pub registrar: HashMap<u32, Vec<String>>
+}
+
+impl School {
+    pub fn new() -> School {
+        School { registrar: HashMap::<u32, Vec<String>>::new() }
+    }
+
+    pub fn add(&mut self, grade: u32, student_name: &str) {
+        let mut students = self.registrar
+            .entry(grade)
+            .or_insert(vec![]);
+        students.push(student_name.to_string());
+        students.sort();
+    }
+
+    pub fn grades(&self) -> Vec<u32> {
+        let mut grades : Vec<u32> = self.registrar.keys()
+            .map(|grade| *grade)
+            .collect();
+        grades.sort();
+        grades
+    }
+
+    pub fn grade<'a>(&'a self, grade: u32) -> Option<&'a Vec<String>> {
+        self.registrar.get(&grade)
+    }
+}
