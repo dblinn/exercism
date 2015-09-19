08491cb133a7e9dd6274356d7100f10cb3b43aad
dblinn <blinnd@gmail.com>
2015-09-19 10:11:19 -0400

    reduce run time in minesweeper problem

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,23 +1,31 @@
 pub fn annotate(board: &Vec<&str>) -> Vec<String> {
-    board.iter().enumerate().map(|(row, line)| {
-        let previous_line = previous(row).map(|i| board[i]);
-        let next_line = next(row, board.len()).map(|i| board[i]);
+    let zero_one_board = convert_to_zero_one(board);
 
-        line.chars().enumerate().map(|(col, c)| {
-            count_neighbors(previous_line, line, next_line, col, c)
+    zero_one_board.iter().enumerate().map(|(row, line)| {
+        let previous_line = previous(row).map(|i| &zero_one_board[i]);
+        let next_line = next(row, board.len()).map(|i| &zero_one_board[i]);
+
+        line.iter().enumerate().map(|(col, n)| {
+            count_neighbors(previous_line, line, next_line, col, *n)
         }).collect()
     }).collect()
 }
 
-fn count_neighbors(previous_line: Option<&str>, cur_line: &str,
-                    next_line: Option<&str>, char_index: usize, c: char) -> char
+fn count_neighbors(previous_line: Option<&Vec<u32>>, cur_line: &Vec<u32>,
+                    next_line: Option<&Vec<u32>>, index: usize, n: u32) -> char
 {
-    if c == '*' { return '*' }
+    if n == 1 { return '*' }
     to_minesweeper_char(
-        line_value(previous_line, char_index) +
-        line_value(Some(cur_line), char_index) - minesweeper_value(Some(c)) +
-        line_value(next_line, char_index)
+        line_value(previous_line, index) +
+        line_value(Some(cur_line), index) +
+        line_value(next_line, index)
     )
+}
+
+fn convert_to_zero_one(board: &Vec<&str>) -> Vec<Vec<u32>> {
+    board.iter().map(|line|
+        line.chars().map(|c| minesweeper_value(c)).collect()
+    ).collect()
 }
 
 fn previous(index: usize) -> Option<usize> {
@@ -28,20 +36,20 @@ fn next(index: usize, max: usize) -> Option<usize> {
     if (index + 1) >= max { None } else { Some(index + 1) }
 }
 
-fn line_value(option_line: Option<&str>, char_index: usize) -> u32 {
+fn line_value(option_line: Option<&Vec<u32>>, index: usize) -> u32 {
     match option_line {
         None => 0,
-        Some(line) => option_lookup(previous(char_index), line) +
-                    minesweeper_value(line.chars().nth(char_index)) +
-                    option_lookup(next(char_index, line.len()), line)
+        Some(line) => option_lookup(previous(index), line) +
+                    line[index] +
+                    option_lookup(next(index, line.len()), line)
     }
 }
 
-fn option_lookup(option_index: Option<usize>, line: &str) -> u32 {
-    option_index.map(|index| minesweeper_value(line.chars().nth(index))).unwrap_or(0)
+fn option_lookup(option_index: Option<usize>, line: &Vec<u32>) -> u32 {
+    option_index.map(|index| line[index]).unwrap_or(0)
 }
 
-fn minesweeper_value(c: Option<char>) -> u32 { match c { Some('*') => 1, _ => 0 }}
+fn minesweeper_value(c: char) -> u32 { match c { '*' => 1, _ => 0 }}
 
 fn to_minesweeper_char(number: u32) -> char {
     match number {
