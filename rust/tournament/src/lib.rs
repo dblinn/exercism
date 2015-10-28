33bb43568e9feec603c83dcb79b2c277b2104a74
dblinn <blinnd@gmail.com>
2015-10-27 21:44:57 -0400

    Refactor tournament solution a bit

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -5,49 +5,49 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug)]
-pub enum Game {
+pub enum GameResult {
 	Loss = 0,
 	Draw = 1,
 	Win = 3
 }
 
-impl Game {
-	pub fn parse(result_string: &str) -> Option<Game> {
+impl GameResult {
+	pub fn parse(result_string: &str) -> Option<GameResult> {
 		match result_string {
-			"loss" => Some(Game::Loss),
-			"draw" => Some(Game::Draw),
-			"win" => Some(Game::Win),
+			"loss" => Some(GameResult::Loss),
+			"draw" => Some(GameResult::Draw),
+			"win" => Some(GameResult::Win),
 			_ => None
 		}
 	}
 
-	pub fn opponent_result(&self) -> Game {
+	pub fn opponent_result(&self) -> GameResult {
 		match self {
-			&Game::Loss => Game::Win,
-			&Game::Draw => Game::Draw,
-			&Game::Win => Game::Loss
+			&GameResult::Loss => GameResult::Win,
+			&GameResult::Draw => GameResult::Draw,
+			&GameResult::Win => GameResult::Loss
 		}
 	}
 
-	pub fn point_for(&self, point_for_result: Game) -> usize {
+	pub fn point_for(&self, point_for_result: GameResult) -> usize {
 		if *self == point_for_result { 1 } else { 0 }
 	}
 }
 
-struct GameOutcome {
+struct GameSummary {
 	pub away_team: String,
 	pub home_team: String,
-	away_team_result: Game
+	away_team_result: GameResult
 }
 
-impl GameOutcome {
-	pub fn away_team_result(&self) -> Game { self.away_team_result }
-	pub fn home_team_result(&self) -> Game { self.away_team_result.opponent_result() }
+impl GameSummary {
+	pub fn away_team_result(&self) -> GameResult { self.away_team_result }
+	pub fn home_team_result(&self) -> GameResult { self.away_team_result.opponent_result() }
 }
 
 #[derive(Eq, Debug)]
 struct TeamRecord {
-    pub record: Vec<Game>
+    pub record: Vec<GameResult>
 }
 
 impl TeamRecord {
@@ -55,26 +55,32 @@ impl TeamRecord {
         TeamRecord { record: vec![] }
     }
 
-	pub fn add_game(&mut self, game: Game) { self.record.push(game); }
+	pub fn add_game(&mut self, game: GameResult) { self.record.push(game); }
 
     pub fn games_played(&self) -> usize {
         self.record.len()
     }
 
+	fn accumulate_record<F>(&self, game_analyzer: F) -> usize
+		where F : Fn(&GameResult) -> usize
+	{
+		self.record.iter().fold(0, |sum, game| sum + game_analyzer(game))
+	}
+
 	pub fn wins(&self) -> usize {
-		self.record.iter().fold(0, |sum, game| sum + game.point_for(Game::Win))
+		self.accumulate_record(|game| game.point_for(GameResult::Win))
 	}
 
 	pub fn losses(&self) -> usize {
-		self.record.iter().fold(0, |sum, game| sum + game.point_for(Game::Loss))
+		self.accumulate_record(|game| game.point_for(GameResult::Loss))
 	}
 
 	pub fn draws(&self) -> usize {
-		self.record.iter().fold(0, |sum, game| sum + game.point_for(Game::Draw))
+		self.accumulate_record(|game| game.point_for(GameResult::Draw))
 	}
 
     pub fn points(&self) -> usize {
-        self.record.iter().cloned().fold(0, |sum, game| sum + game as usize)
+		self.accumulate_record(|game| *game as usize)
     }
 }
 
@@ -94,27 +100,16 @@ impl Ord for TeamRecord {
     }
 }
 
-struct Parser;
-impl Parser {
+struct StandingsParser;
+impl StandingsParser {
 	fn parse(file: File) -> HashMap<String, TeamRecord> {
 		let mut records = HashMap::<String, TeamRecord>::new();
 		let reader = BufReader::new(file);
+
 		for score_result in reader.lines() {
 			match Self::parse_result(score_result) {
 				Some(outcome) => {
-					let (home_result, away_result, home_team, away_team) = (
-						outcome.home_team_result(),
-						outcome.away_team_result(),
-						outcome.home_team,
-						outcome.away_team
-					);
-
-					records.entry(home_team)
-						.or_insert(TeamRecord::new())
-						.add_game(home_result);
-					records.entry(away_team)
-						.or_insert(TeamRecord::new())
-						.add_game(away_result);
+					Self::add_outcome(outcome, &mut records)
 				},
 				_ => {}
 			}
@@ -123,18 +118,28 @@ impl Parser {
 		records
 	}
 
-	fn parse_result(score_result: Result<String>) -> Option<GameOutcome> {
+	fn add_outcome(outcome: GameSummary, records: &mut HashMap<String, TeamRecord>) {
+		let (home_result, away_result) = (
+			outcome.home_team_result(),
+			outcome.away_team_result()
+		);
+
+		records.entry(outcome.home_team).or_insert(TeamRecord::new()).add_game(home_result);
+		records.entry(outcome.away_team).or_insert(TeamRecord::new()).add_game(away_result);
+	}
+
+	fn parse_result(score_result: Result<String>) -> Option<GameSummary> {
 		match score_result {
 			Err(_) => None,
-			Ok(ref score_line) => Self::parse_outcome(score_line)
+			Ok(ref score_line) => Self::parse_game_summary(score_line)
 		}
 	}
 
-	fn parse_outcome(score_line: &str) -> Option<GameOutcome> {
+	fn parse_game_summary(score_line: &str) -> Option<GameSummary> {
 		if score_line.starts_with('#') { return None }
 		let fields = score_line.split(';').collect::<Vec<&str>>();
 		if fields.len() != 3 { return None }
-		Game::parse(fields[2]).map(|game| GameOutcome {
+		GameResult::parse(fields[2]).map(|game| GameSummary {
 			away_team: fields[0].to_string(),
 			home_team: fields[1].to_string(),
 			away_team_result: game
@@ -142,13 +147,16 @@ impl Parser {
 	}
 }
 
+const STANDINGS_HEADER: &'static str =
+	"Team                           | MP |  W |  D |  L |  P\n";
+
 struct Standings {
     records: HashMap<String, TeamRecord>
 }
 
 impl Standings {
     pub fn from_file(file: File) -> Standings {
-        Standings { records: Parser::parse(file) }
+        Standings { records: StandingsParser::parse(file) }
     }
 
     pub fn sorted_teams(&self) -> Vec<(&String, &TeamRecord)> {
@@ -162,7 +170,7 @@ impl Standings {
 		records
     }
 
-	pub fn pretty_print(team_name: &str, team_record: &TeamRecord) -> String {
+	pub fn pretty_format(team_name: &str, team_record: &TeamRecord) -> String {
 		format!("{:30} | {:2} | {:2} | {:2} | {:2} | {:2}\n",
 			team_name,
 			team_record.games_played(),
@@ -171,19 +179,6 @@ impl Standings {
 			team_record.losses(),
 			team_record.points())
 	}
-
-    pub fn write_outcome(&self, file: File) {
-		let teams = self.sorted_teams();
-		let mut writer = BufWriter::new(file);
-		writer.write("Team                           | MP |  W |  D |  L |  P\n".as_bytes()).unwrap();
-
-		for (team_name, team_record) in teams {
-			// let line = format!("{:?} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n",
-			// writer.write()
-			writer.write(Self::pretty_print(team_name, team_record).as_bytes()).unwrap();
-		}
-		writer.flush().unwrap();
-    }
 
     pub fn games_played(&self) -> usize {
 		// Games are double counted, since two teams play in each game
@@ -195,19 +190,23 @@ impl Standings {
 }
 
 pub fn tally(input_path: &Path, output_path: &Path) -> Option<usize> {
-    let input_file = open_file(File::open(input_path), input_path);
-    let output_file = open_file(File::create(output_path), output_path);
-    if input_file.is_none() || output_file.is_none() { return None }
+    let input_file = File::open(input_path).ok()
+		.expect("Failed to open input file for reading");
+    let output_file = File::create(output_path).ok()
+		.expect("Failed to open output file for writing");
 
-    let standings = Standings::from_file(input_file.unwrap());
-    standings.write_outcome(output_file.unwrap());
+    let standings = Standings::from_file(input_file);
+	write_standings(&standings, output_file);
 
     return Some(standings.games_played());
 }
 
-fn open_file(file_result: Result<File>, path: &Path) -> Option<File> {
-    match file_result {
-        Err(e) => { println!("Couldn't open {:?}: {}", path.file_name(), e); None },
-        Ok(f) => Some(f)
-    }
+fn write_standings(standings: &Standings, file: File) {
+	let teams = standings.sorted_teams();
+	let mut writer = BufWriter::new(file);
+	writer.write(STANDINGS_HEADER.as_bytes()).unwrap();
+
+	for (team_name, team_record) in teams {
+		writer.write(Standings::pretty_format(team_name, team_record).as_bytes()).unwrap();
+	}
 }
